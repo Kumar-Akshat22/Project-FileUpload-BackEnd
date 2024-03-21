0304cbef19e6ece4b1da86e2fb74dd8c29fb9117
Kumar Akshat <kakshat247@gmail.com>
2024-03-21 22:30:23 +0530

    implemented the controller that reduces the size of the image and uploads it to cloudinary

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -45,10 +45,17 @@ function isFileTypeSupported(type , supportedFileTypes){
 
 }
 
-async function uploadFileToCloudinary(file , folder){
+async function uploadFileToCloudinary(file , folder , quality){
 
     const options = {folder};
     console.log("tempFilePath",file.tempFilePath);
+    
+    if(quality){
+
+        options.quality = quality;
+
+    }
+
     options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 
@@ -188,6 +195,72 @@ exports.videoUpload = async(req,res) =>{
             message:'Something went wrong'
         })
 
+    }
+}
+
+// Image Size Reduceer and Upload controller
+exports.imageSizeReducer = async(req,res) => {
+
+    try{
+
+        // Fetch the data from request 
+        const { name, tags, email } = req.body;
+        console.log(name,tags,email);
+
+        // Fetch the Image from the request body
+        const file = req.files.Image;
+        
+        // TODO: add an upper limit of size to the image
+        // TODO: reduce the size of the image by adjusting height attribute
+        // Validation
+        const fileType = path.extname(file.name).toLowerCase();
+        console.log(fileType);
+
+        const supportedFileTypes = [".jpg",".jpeg",".png"];
+
+        // Check whether the file type is supported or not
+        if(!isFileTypeSupported(fileType , supportedFileTypes)){
+
+            return res.status(400).json({
+
+                success:false,  
+                message:"File Type not Supported"
+            })
+        }
+
+        // File Format is Supported
+        // Upload to Cloudinary
+        console.log('Uploading to Project-FileUpload on Cloudinary');
+        const response = await uploadFileToCloudinary(file, "Project-FileUpload",90);
+        console.log(response);
+
+        // Save the entry into the database
+        const fileData = await File.create({
+
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        });
+
+        res.status(200).json({
+            success:true,
+            // imageUrl:response.secure_url,
+            // message:"Image successfully uploaded"
+        })
 
     }
+
+    catch(err){
+        
+        console.error(err);
+        res.status(400).json({
+
+            success:false,
+            message:'Something went wrong'
+        })
+
+    }
+
+
 }
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFileUpload,imageUpload,videoUpload} = require("../controllers/fileUpload");
+const {localFileUpload,imageUpload,videoUpload,imageSizeReducer} = require("../controllers/fileUpload");
 
 // API route
 router.post('/localFileUpload' , localFileUpload);
 router.post('/imageUpload', imageUpload);
-router.post('/videoUpload',videoUpload)
+router.post('/videoUpload',videoUpload);
+router.post('/imageSizeReducer' , imageSizeReducer);
 
 module.exports = router;
 
