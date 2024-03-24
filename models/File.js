9434e1b585cbf9e7dc4c994c10060af1680d97d7
Kumar Akshat <kakshat247@gmail.com>
2024-03-24 13:04:59 +0530

    implemented the functionality to send email to user on uploading a file to cloudinary and successfully completed the FileUpload backend project

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
 
@@ -25,5 +27,43 @@ const fileSchema = new mongoose.Schema({
     }
 });
 
+// Post Middleware
+fileSchema.post('save' , async function(doc){
+
+    try{
+
+        console.log('DOC->',doc);
+
+        // Nodemailer Transporter
+        // TODO: Shift this configuration under /config folder
+        let transporter = nodemailer.createTransport({
+
+            host: process.env.MAIL_HOST,
+            auth:{
+
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+
+            }
+        });
+
+        // Now, send the mail
+        let info = await transporter.sendMail({
+
+            from: "MyServer",
+            to: doc.email,
+            subject: "New file uploaded to Cloudinary",
+            html: `<h2>Hello, Guys</h2> <p>File Uploaded <a href="${doc.imageUrl}">Click Here</a></p>`,
+
+        });
+
+        console.log("INFO->",info)
+    }
+
+    catch(error){
+
+        console.error(error);
+    }
+})
 const File = mongoose.model("File",fileSchema);
 module.exports = File;
